0f4f556f9694f16fa08198592075d56829c0b8e2
Patitapaban <patitapaban.m93@gmail.com>
2023-06-28 10:43:45 +0530

    Initial commit containing project template

---
+++ src/test/java/com/banking/application/corebankingservice/CoreBankingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.application.corebankingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreBankingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
